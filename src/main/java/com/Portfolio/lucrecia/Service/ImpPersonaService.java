5371ec40b0a9fd7c970712225b4a18080ab5b562
lukhoury <66232483+lukhoury@users.noreply.github.com>
2023-05-15 18:13:17 -0300

    Add files via upload

---
+++ src/main/java/com/Portfolio/lucrecia/Service/ImpPersonaService.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Portfolio.lucrecia.Service;
+
+import com.Portfolio.lucrecia.Entity.Persona;
+import com.Portfolio.lucrecia.Interface.IPersonaService;
+import com.Portfolio.lucrecia.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService  implements IPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+       Persona persona = ipersonaRepository.findById(id).orElse(null);
+       return persona;
+    }
+    
+}
