d0610b06d2b18a315382839e26657603ac7fb50d
lukhoury <lucreciakhoury@gmail.com>
2023-05-15 17:10:17 -0300

    BackEnd

---
+++ lucrecia/lucrecia/src/test/java/com/Portfolio/lucrecia/LucreciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.lucrecia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucreciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
