e1e163122d6c61933a869b5ca8397682ced0f507
lukhoury <lucreciakhoury@gmail.com>
2023-05-16 00:10:36 -0300

    ultima correcion

--- src/main/java/com/Portfolio/lucrecia/Controller/CExperiencia.java
+++ src/main/java/com/Portfolio/lucrecia/Controller/CExperiencia.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"https://portfolio-d9633.web.app"})
+@CrossOrigin(origins = {"https://portfolio-d9633.web.app", "http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/Portfolio/lucrecia/Security/Controller/AuthController.java
+++ src/main/java/com/Portfolio/lucrecia/Security/Controller/AuthController.java
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://portfolio-d9633.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfolio-d9633.web.app","'http://localhost:4040'"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
